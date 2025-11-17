bc639bdc4244da1f388c417935815e9561fd2ff0
GuaGua0216 <171306165+GuaGua0216@users.noreply.github.com>
2025-11-17 11:25:18 +0800

    下載capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.guagua.clipboard.mobile',
+  appName: 'GuaguaClipboard',
+  webDir: 'dist'
+};
+
+export default config;
